03a0cdeb8d17a16dcf08071052fa5d0e221d9259
wakana <greentea.mmlg.0405@gmail.com>
2022-08-14 16:23:18 +0900

    add testSetUp

--- xunit.py
+++ xunit.py
@@ -1,32 +1,35 @@
-from time import thread_time_ns
-
-
 class TestCase:
-    def __init__(self, name) :
+    def __init__(self, name):
         self.name = name
-    def run(self) :
+    def setUp(self):
+        pass
+    def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 class WasRun(TestCase):
-    def __init__(self, name) :
+    def setUp(self):
         self.wasRun = None
-        super().__init__(name)
-
-    def testMethod(self) :
+        self.wasSetUp = 1
+    def testMethod(self):
         self.wasRun = 1
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
 
 # [x]テストメソッドを呼び出す
-# [ ]setUpを最初に呼び出す
+# [x]setUpを最初に呼び出す
 # [ ]tearDownを後で呼び出す
 # [ ]テストメソッドが失敗したとしてもtearDown()を呼び出す
 # [ ]複数のテストを走らせる
