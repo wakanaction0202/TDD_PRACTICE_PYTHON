f2853242d2263b24b2bb7920f275d848d2076feb
wakana <greentea.mmlg.0405@gmail.com>
2022-08-14 15:30:35 +0900

    test green

--- xunit.py
+++ xunit.py
@@ -1,8 +1,10 @@
+class WasRun:
+    def __init__(self, name) :
+        self.wasRun = None
+    def testMethod(self) :
+        self.wasRun = 1
+
 test = WasRun("testMethod")
 print(test.wasRun)
 test.testMethod()
 print(test.wasRun)
-
-
-class WasRun:
-    pass
