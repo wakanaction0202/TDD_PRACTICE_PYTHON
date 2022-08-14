eec44f01ce0d3d8d4397ef9eba5515597797bbd7
wakana <greentea.mmlg.0405@gmail.com>
2022-08-14 15:46:45 +0900

    add TestCaseTest

--- xunit.py
+++ xunit.py
@@ -1,10 +1,12 @@
+from time import thread_time_ns
+
+
 class TestCase:
     def __init__(self, name) :
         self.name = name
     def run(self) :
         method = getattr(self, self.name)
         method()
-
 
 class WasRun(TestCase):
     def __init__(self, name) :
@@ -14,7 +16,11 @@ class WasRun(TestCase):
     def testMethod(self) :
         self.wasRun = 1
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+        
+TestCaseTest("testRunning").run()
