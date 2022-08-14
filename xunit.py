a9776e807df890d9ee9e2d558fa68caea60f3012
wakana <greentea.mmlg.0405@gmail.com>
2022-08-14 15:41:29 +0900

    add TestCase

--- xunit.py
+++ xunit.py
@@ -1,8 +1,16 @@
-class WasRun:
+class TestCase:
+    def __init__(self, name) :
+        self.name = name
+    def run(self) :
+        method = getattr(self, self.name)
+        method()
+
+
+class WasRun(TestCase):
     def __init__(self, name) :
         self.wasRun = None
-    def run(self) :
-        self.testMethod()
+        super().__init__(name)
+
     def testMethod(self) :
         self.wasRun = 1
 
